114d519861963ce0268439c721d0d3047c381864
Immortal <you@example.com>
2022-10-13 13:53:34 -0700

    4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-  #include <stdio.h>
+#include <stdio.h>
 
-  /**
-   * main - prints the phrase "Programming is like building a multilingual puzzle
-   */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ */
 
-  int main(void)
-  {
-          puts("\"Programming is like building a multilingual puzzle");
-          return (0);
-  }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
