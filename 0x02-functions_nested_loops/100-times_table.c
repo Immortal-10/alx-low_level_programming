26f174cd76e1f6446897c47349610d4549aa16fe
Immortal <you@example.com>
2022-10-19 14:57:06 -0700

    y

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,19 +13,19 @@ int a, b, op;
 
 if (n >= 0 && n <= 15)
 {
-_putchar(48);
-for (b = 1; b <= n; b++)
-{
 for (a = 0; a <= n; a++)
+{
+_putchar(48)
+for (b = 1; b <= n; b++)
 {
 op = a * b;
 _putchar(44);
 _putchar(32);
-if (op <= 9);
+ if (op <=9)
 {
 _putchar(32);
 _putchar(32);
-_putchar(op + 48)
+_putchar(op + 48);
 }
 else if (op <= 99)
 {
