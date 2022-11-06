89d3d17005339bb421a020434a635e3974275d34
Immortal <you@example.com>
2022-11-05 18:36:09 -0700

    u

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints
+ * @argc: number
+ * @argv: array
+ * Return: 0 - Success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
