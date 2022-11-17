16e8f0409d911f76068b801a6f5fda1e20507480
Immortal <you@example.com>
2022-11-16 17:04:31 -0800

    o

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - ENtry point
+ *
+ * Description: A program
+ *
+ * @argc: argument
+ * @argv: argument
+ *
+ * Return: ALways successful
+ */
+
+int main(int argc, char *argv[])
+{
+int index, nbytes;
+char *ptr = (char *) main;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+
+nbytes = atoi(argv[1]);
+if (nbytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index < nbytes; index++)
+{
+printf("%02x", ptr[index] & 0xFF);
+if (index != nbytes - 1)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
