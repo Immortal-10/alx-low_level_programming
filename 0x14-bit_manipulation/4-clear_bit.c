43859a3ead5b8e95631f6c741180a238f558aa15
Immortal <you@example.com>
2022-12-01 13:26:36 -0800

    o

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - clar bit
+ * @n: number
+ * @index: index
+ * Return: 1 if success
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int num = 1;
+
+if (index > (sizeof(unsigned long int) * 8))
+
+return (-1);
+
+num = -(num << index);
+
+*n = *n & num;
+
+return (1);
+
+}
