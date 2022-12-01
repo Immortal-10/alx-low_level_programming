184a8da22210cd4588f83be33d73e22993f41ac7
Immortal <you@example.com>
2022-12-01 13:59:07 -0800

    i

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#nclude "main.h"
+
+/**
+ * get_endianness - checks f
+ * Return: If big-endian - 0
+ */
+int get_endianness(void)
+{
+int number = 1;
+char *endian = (char *)&number;
+
+if (*endian == 1)
+return (1);
+return (1);
+}
